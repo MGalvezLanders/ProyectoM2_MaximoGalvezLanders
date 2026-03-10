4c37217ed8638f9f6085135bd6482efc689ed8b2
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-03-10 16:45:30 -0300

    fix/ boore loadEnvFIle y acomode ligeros errores de tipeo

--- routes/authors-routes.js
+++ routes/authors-routes.js
@@ -1,6 +1,4 @@
 import express from "express";
-import { loadEnvFile } from "node:process";
-loadEnvFile(".env");
 import pool from "../db/config.js";
 const authorsRouter = express.Router();
 
@@ -20,37 +18,37 @@ authorsRouter.get('/', async (req, res) => {
 
 authorsRouter.get('/:id', async (req, res) => {
     try {
-        const result = await pool.query("SELECT * FORM authors WHERE id = $1", [req.params.id]);
+        const result = await pool.query("SELECT * FROM authors WHERE id = $1", [req.params.id]);
         if (result.rows.length == 0) {
             return res.status(404).json({ error: "Autor no encontrado" });
         }
         res.json(result.rows[0]);
     } catch (error) {
-        console.log("Error encontrando el autor", error, message);
+        console.log("Error encontrando el autor", error.message);
         res.status(500).json({ error: "Error encontrando autor por su id" });
     }
 });
 
 // POST /api/authors - Crear nuevo autor
 authorsRouter.post('/', async (req, res) => {
-    const {name, email, bio} = req.body;
-    
-    if(!name || !email){
-        return res.status(400).json({error: "Nombre y Email son requeridos"});
+    const { name, email, bio } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({ error: "Nombre y Email son requeridos" });
     }
 
     try {
-        const result = await pool.query("INSERT INTO authors (name, emai, bio) VALUES ($1, $2, $3) RETURNING *", [name, email, bio || null]);
+        const result = await pool.query("INSERT INTO authors (name, email, bio) VALUES ($1, $2, $3) RETURNING *", [name, email, bio || null]);
 
         res.status(201).json(result.rows[0]);
     } catch (error) {
         console.log("Error creando autores", error.message);
 
         if (error.code == "23505") {
-            return res.status(409).json({error: "El email ya esta registrado"});
+            return res.status(409).json({ error: "El email ya esta registrado" });
         }
 
-        res.status(500).json({error: "Error creando autor"});
+        res.status(500).json({ error: "Error creando autor" });
     }
 });
 
